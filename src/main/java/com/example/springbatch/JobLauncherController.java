e586c3b3622c0173636d3521157b9fb1dd4ae588
jaehee <jaehee23@naver.com>
2023-11-01 15:17:23 +0900

    jobLauncher

---
+++ src/main/java/com/example/springbatch/JobLauncherController.java
@@ -0,0 +1,37 @@
+package com.example.springbatch;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+public class JobLauncherController {
+
+    private final Job job;
+    private final JobLauncher jobLauncher;
+
+    @PostMapping("/batch")
+    public String launch(@RequestBody Member member) throws Exception {
+
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addString("member", member.getId())
+                .addDate("date", new Date())
+                .toJobParameters();
+
+        jobLauncher.run(job, jobParameters);
+
+        return "batch completed";
+    }
+
+}
