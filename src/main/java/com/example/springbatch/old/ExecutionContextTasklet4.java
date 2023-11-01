285cb5b31d152fdb7c2a20ad5f2bb8e7e9668abc
jaehee <jaehee23@naver.com>
2023-11-01 15:03:51 +0900

    Initial commit

---
+++ src/main/java/com/example/springbatch/old/ExecutionContextTasklet4.java
@@ -0,0 +1,19 @@
+package com.example.springbatch.old;
+
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.stereotype.Component;
+
+
+public class ExecutionContextTasklet4 implements Tasklet {
+    @Override
+    public RepeatStatus execute(final StepContribution stepContribution, final ChunkContext chunkContext) throws Exception {
+
+        System.out.println("name:" + chunkContext.getStepContext().getStepExecution().getJobExecution().getExecutionContext().get("name"));
+        System.out.println("step4 was executed");
+
+        return RepeatStatus.FINISHED;
+    }
+}
---
+++ src/main/java/com/example/springbatch/old/JobRepositoryListener.java
@@ -0,0 +1,40 @@
+package com.example.springbatch.old;
+
+import org.springframework.batch.core.*;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+//@Component
+public class JobRepositoryListener implements JobExecutionListener {
+
+    @Autowired
+    private JobRepository jobRepository;
+
+
+    @Override
+    public void beforeJob(final JobExecution jobExecution) {
+
+    }
+
+    @Override
+    public void afterJob(final JobExecution jobExecution) {
+        String jobName = jobExecution.getJobInstance().getJobName();
+
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addString("requestDate", "20210101").toJobParameters();
+
+        JobExecution lastJobExecution = jobRepository.getLastJobExecution(jobName, jobParameters);
+        if(lastJobExecution != null){
+            for (StepExecution execution : lastJobExecution.getStepExecutions()){
+                BatchStatus status = execution.getStatus();
+                System.out.println("status: " + status);
+                ExitStatus exitStatus = execution.getExitStatus();
+                System.out.println("exitStatus: " + exitStatus);
+                String stepName = execution.getStepName();
+                System.out.println("stepName: " + stepName);
+            }
+        }
+
+    }
+}
