5aff3721c3cdae72ae241561c8e7f04ed9463ccd
jaehee <jaehee23@naver.com>
2023-11-01 15:25:15 +0900

    jobLauncher async

--- src/main/java/com/example/springbatch/JobLauncherController.java
+++ src/main/java/com/example/springbatch/JobLauncherController.java
@@ -6,6 +6,9 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.launch.support.SimpleJobLauncher;
+import org.springframework.boot.autoconfigure.batch.BasicBatchConfigurer;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +23,7 @@ public class JobLauncherController {
 
     private final Job job;
     private final JobLauncher jobLauncher;
+    private final BasicBatchConfigurer basicBatchConfigurer;
 
     @PostMapping("/batch")
     public String launch(@RequestBody Member member) throws Exception {
@@ -29,7 +33,9 @@ public class JobLauncherController {
                 .addDate("date", new Date())
                 .toJobParameters();
 
-        jobLauncher.run(job, jobParameters);
+        SimpleJobLauncher jobLauncher1 = (SimpleJobLauncher) basicBatchConfigurer.getJobLauncher();
+        jobLauncher1.setTaskExecutor(new SimpleAsyncTaskExecutor());
+        jobLauncher1.run(job, jobParameters);
 
         return "batch completed";
     }
