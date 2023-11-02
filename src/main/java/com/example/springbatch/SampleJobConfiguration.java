ef0b41f9fb658878ebba118805fd895ee65931dc
jaehee <jaehee23@naver.com>
2023-11-02 11:21:31 +0900

    JobBuilder

--- src/main/java/com/example/springbatch/SampleJobConfiguration.java
+++ src/main/java/com/example/springbatch/SampleJobConfiguration.java
@@ -1,11 +1,10 @@
 package com.example.springbatch;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.Step;
-import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.*;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
@@ -25,6 +24,26 @@ public class SampleJobConfiguration {
         return this.jobBuilderFactory.get("Job")
                 .start(step1())
                 .next(step2())
+                .next(step3())
+                .incrementer(new RunIdIncrementer())
+                .validator(new JobParametersValidator() {
+                    @Override
+                    public void validate(final JobParameters parameters) throws JobParametersInvalidException {
+
+                    }
+                })
+                .preventRestart()
+                .listener(new JobExecutionListener() {
+                    @Override
+                    public void beforeJob(final JobExecution jobExecution) {
+
+                    }
+
+                    @Override
+                    public void afterJob(final JobExecution jobExecution) {
+
+                    }
+                })
                 .build();
     }
 
@@ -42,6 +61,18 @@ public class SampleJobConfiguration {
         return stepBuilderFactory.get("step2")
                 .tasklet((contribution, chunkContext) -> {
                     System.out.println("step2 has executed");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    @Bean
+    public Step step3() {
+        return stepBuilderFactory.get("step3")
+                .tasklet((contribution, chunkContext) -> {
+                    chunkContext.getStepContext().getStepExecution().setStatus(BatchStatus.FAILED);
+                    contribution.setExitStatus(ExitStatus.STOPPED);
+                    System.out.println("step3 has executed");
                     return RepeatStatus.FINISHED;
                 })
                 .build();
