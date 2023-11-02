d19ad5bd90fbe2ff7bcdfc0103b520e45e98be6c
jaehee <jaehee23@naver.com>
2023-11-02 10:35:06 +0900

    기본 데이터

--- src/main/java/com/example/springbatch/SampleJobConfiguration.java
+++ src/main/java/com/example/springbatch/SampleJobConfiguration.java
@@ -3,12 +3,16 @@ package com.example.springbatch;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@Configuration
 @RequiredArgsConstructor
 public class SampleJobConfiguration {
 
@@ -17,8 +21,8 @@ public class SampleJobConfiguration {
     private final StepBuilderFactory stepBuilderFactory;
 
     @Bean
-    public Job job(){
-        return jobBuilderFactory.get("Job")
+    public Job BatchJob() {
+        return this.jobBuilderFactory.get("Job")
                 .start(step1())
                 .next(step2())
                 .build();
@@ -28,17 +32,16 @@ public class SampleJobConfiguration {
     public Step step1() {
         return stepBuilderFactory.get("step1")
                 .tasklet((contribution, chunkContext) -> {
-                    System.out.println("step1 was excuted");
+                    System.out.println("step1 has executed");
                     return RepeatStatus.FINISHED;
                 })
                 .build();
     }
-
     @Bean
     public Step step2() {
         return stepBuilderFactory.get("step2")
                 .tasklet((contribution, chunkContext) -> {
-                    System.out.println("step2 was excuted");
+                    System.out.println("step2 has executed");
                     return RepeatStatus.FINISHED;
                 })
                 .build();
