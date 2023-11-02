215c160a66df08f1112d3a24e067d29390986033
jaehee <jaehee23@naver.com>
2023-11-02 15:19:16 +0900

    자동 증가 파라미터

--- src/main/java/com/example/springbatch/SampleJobConfiguration.java
+++ src/main/java/com/example/springbatch/SampleJobConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
@@ -23,6 +24,7 @@ public class SampleJobConfiguration {
     @Bean
     public Job BatchJob() {
         return this.jobBuilderFactory.get("Job")
+                .incrementer(new RunIdIncrementer())
                 .start(step1())
                 .next(step2())
                 .build();
