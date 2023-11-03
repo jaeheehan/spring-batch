e12f131c5e04908ee399dc184b80446dc848ed91
jaehee <jaehee23@naver.com>
2023-11-03 15:45:34 +0900

    simpleFlow

--- src/main/java/com/example/springbatch/SampleJobConfiguration.java
+++ src/main/java/com/example/springbatch/SampleJobConfiguration.java
@@ -1,11 +1,14 @@
 package com.example.springbatch;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.builder.FlowBuilder;
+import org.springframework.batch.core.job.flow.Flow;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
@@ -22,11 +25,38 @@ public class SampleJobConfiguration {
     private final StepBuilderFactory stepBuilderFactory;
 
     @Bean
-    public Job BatchJob() {
-        return this.jobBuilderFactory.get("Job")
-                .incrementer(new RunIdIncrementer())
+    public Job flowJob() {
+        return this.jobBuilderFactory.get("flowJob")
+                .start(flow1())
+                .on("COMPLETED").to(flow2())
+                .from(flow1())
+                .on("FAILED").to(flow3())
+                .end()
+                .build();
+    }
+
+    @Bean
+    public Flow flow1() {
+        return new FlowBuilder<Flow>("flow1")
                 .start(step1())
                 .next(step2())
+                .build();
+    }
+
+    @Bean
+    public Flow flow2() {
+        return new FlowBuilder<Flow>("flow1")
+                .start(flow3())
+                .next(step5())
+                .next(step6())
+                .build();
+    }
+
+    @Bean
+    public Flow flow3() {
+        return new FlowBuilder<Flow>("flow1")
+                .start(step3())
+                .next(step4())
                 .build();
     }
 
@@ -44,6 +74,46 @@ public class SampleJobConfiguration {
         return stepBuilderFactory.get("step2")
                 .tasklet((contribution, chunkContext) -> {
                     System.out.println("step2 has executed");
+                    contribution.getStepExecution().setExitStatus(ExitStatus.FAILED);
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    @Bean
+    public Step step3() {
+        return stepBuilderFactory.get("step3")
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("step3 has executed");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    @Bean
+    public Step step4() {
+        return stepBuilderFactory.get("step4")
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("step4 has executed");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+    @Bean
+    public Step step5() {
+        return stepBuilderFactory.get("step5")
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("step5 has executed");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    @Bean
+    public Step step6() {
+        return stepBuilderFactory.get("step6")
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("step6 has executed");
                     return RepeatStatus.FINISHED;
                 })
                 .build();
