3582869084097bb94c5bbab57675475a1eaff447
jaehee <jaehee23@naver.com>
2023-11-03 10:28:27 +0900

    jobStep

--- src/main/java/com/example/springbatch/SampleJobConfiguration.java
+++ src/main/java/com/example/springbatch/SampleJobConfiguration.java
@@ -1,13 +1,14 @@
 package com.example.springbatch;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.Step;
-import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.*;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.job.DefaultJobParametersExtractor;
+import org.springframework.batch.core.step.job.JobParametersExtractor;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
@@ -22,11 +23,45 @@ public class SampleJobConfiguration {
     private final StepBuilderFactory stepBuilderFactory;
 
     @Bean
-    public Job BatchJob() {
-        return this.jobBuilderFactory.get("Job")
-                .incrementer(new RunIdIncrementer())
-                .start(step1())
+    public Job ParentJob() {
+        return this.jobBuilderFactory.get("ParentJob")
+                //.incrementer(new RunIdIncrementer())
+                .start(jobStep(null))
                 .next(step2())
+                .build();
+    }
+
+    @Bean
+    public Step jobStep(JobLauncher jobLauncher) {
+        return stepBuilderFactory.get("jobStep")
+                .job(childJob())
+                .launcher(jobLauncher)
+                .parametersExtractor(jobParameterExtractor())
+                .listener(new StepExecutionListener() {
+                    @Override
+                    public void beforeStep(final StepExecution stepExecution) {
+                         stepExecution.getExecutionContext().putString("name", "user1");
+                    }
+
+                    @Override
+                    public ExitStatus afterStep(final StepExecution stepExecution) {
+                        return null;
+                    }
+                })
+                .build();
+    }
+
+    private DefaultJobParametersExtractor jobParameterExtractor() {
+        DefaultJobParametersExtractor extractor = new DefaultJobParametersExtractor();
+        extractor.setKeys(new String[] {"name"});
+        return extractor;
+    }
+
+
+    @Bean
+    public Job childJob() {
+        return jobBuilderFactory.get("childJob")
+                .start(step1())
                 .build();
     }
 
