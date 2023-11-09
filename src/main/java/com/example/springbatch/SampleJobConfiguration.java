d57388c80e2e5e366901ed103415c9a23cddaefe
jaehee <jaehee23@naver.com>
2023-11-09 14:32:47 +0900

    multi processing - async Item processor

--- src/main/java/com/example/springbatch/SampleJobConfiguration.java
+++ src/main/java/com/example/springbatch/SampleJobConfiguration.java
@@ -5,13 +5,22 @@ import org.springframework.batch.core.*;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
-import org.springframework.batch.item.ItemReader;
-import org.springframework.batch.item.NonTransientResourceException;
-import org.springframework.batch.item.ParseException;
-import org.springframework.batch.item.UnexpectedInputException;
+import org.springframework.batch.integration.async.AsyncItemProcessor;
+import org.springframework.batch.integration.async.AsyncItemWriter;
+import org.springframework.batch.item.*;
 import org.springframework.batch.item.adapter.ItemWriterAdapter;
+import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.database.JdbcPagingItemReader;
+import org.springframework.batch.item.database.Order;
+import org.springframework.batch.item.database.support.MySqlPagingQueryProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 @RequiredArgsConstructor
@@ -20,44 +29,109 @@ public class SampleJobConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
+    private final DataSource dataSource;
 
     @Bean
     public Job job() throws Exception {
         return jobBuilderFactory.get("batchJob")
                 .incrementer(new RunIdIncrementer())
                 .start(step1())
+                //.start(asyncStep1())
+                .listener(new StopWatchJobListener())
                 .build();
     }
 
     @Bean
     public Step step1() throws Exception {
         return stepBuilderFactory.get("step1")
-                .<String, String>chunk(10)
-                .reader(new ItemReader<String>() {
-                    int i = 0;
-                    @Override
-                    public String read() throws Exception, UnexpectedInputException, ParseException, NonTransientResourceException {
-                        i++;
-                        return i > 10 ? null : "item" + i;
-                    }
-                })
+                .<Customer, Customer>chunk(10)
+                .reader(pagingItemReader())
+                .processor(customItemProcessor())
                 .writer(customItemWriter())
                 .build();
     }
 
-
-
     @Bean
-    public ItemWriterAdapter<String> customItemWriter() {
+    public ItemProcessor<Customer, Customer> customItemProcessor() throws InterruptedException {
+        return new ItemProcessor<Customer, Customer>() {
+            @Override
+            public Customer process(final Customer item) throws Exception {
 
-        ItemWriterAdapter<String>  writer = new ItemWriterAdapter<>();
-        writer.setTargetObject(customService());
-        writer.setTargetMethod("joinCustomer");
-        return  writer;
+                Thread.sleep(30);
+                return new Customer(item.getId(), item.getFirstName().toUpperCase(), item.getLastName().toUpperCase(), item.getBirthdate());
+            }
+        };
     }
 
     @Bean
-    public CustomService customService() {
-        return new CustomService();
+    public Step asyncStep1() throws Exception{
+        return stepBuilderFactory.get("asyncStep1")
+                .<Customer, Customer>chunk(100)
+                .reader(pagingItemReader())
+                .processor(asyncItemProcessor())
+                .writer(asyncItemWriter())
+                .build();
     }
+
+
+    @Bean
+    public AsyncItemProcessor asyncItemProcessor() throws Exception{
+
+        AsyncItemProcessor<Customer, Customer> asyncItemProcessor = new AsyncItemProcessor<>();
+        asyncItemProcessor.setDelegate(customItemProcessor());
+        asyncItemProcessor.setTaskExecutor(new SimpleAsyncTaskExecutor());
+
+        return asyncItemProcessor;
+    }
+
+    @Bean
+    public AsyncItemWriter<Customer> asyncItemWriter() throws Exception{
+
+        AsyncItemWriter<Customer> asyncItemWriter = new AsyncItemWriter<>();
+        asyncItemWriter.setDelegate(customItemWriter());
+
+
+        return asyncItemWriter;
+    }
+
+    @Bean
+    public JdbcPagingItemReader<Customer> pagingItemReader() {
+
+        JdbcPagingItemReader<Customer> reader = new JdbcPagingItemReader<>();
+
+        reader.setDataSource(dataSource);
+        reader.setFetchSize(300);
+        reader.setRowMapper(new CustomerRowMapper());
+
+
+        MySqlPagingQueryProvider queryProvider = new MySqlPagingQueryProvider();
+        queryProvider.setSelectClause("id, firstName, lastName, birthdate");
+        queryProvider.setFromClause("from customer");
+
+        Map<String, Order> sortKeys = new HashMap<>(1);
+        sortKeys.put("id", Order.ASCENDING);
+
+        queryProvider.setSortKeys(sortKeys);
+
+        reader.setQueryProvider(queryProvider);
+        return reader;
+
+    }
+
+
+
+    @Bean
+    public JdbcBatchItemWriter<Customer> customItemWriter() {
+
+        JdbcBatchItemWriter<Customer> itemWriter = new JdbcBatchItemWriter<>();
+
+        itemWriter.setDataSource(dataSource);
+        itemWriter.setSql("INSERT INTO customer2 VALUES (:id, :firstName, :lastName, :birthdate)");
+        itemWriter.setItemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<>());
+        itemWriter.afterPropertiesSet();
+
+        return itemWriter;
+    }
+
+
 }
